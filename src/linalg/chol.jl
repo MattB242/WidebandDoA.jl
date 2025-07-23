74f41029a68cd3f6eb3ffe33fbeb8aca9aa8d282
MattB242 <MattB242@liverpool.ac.uk>
2025-07-23 16:27:23 +0100

    Add GPU versions of collapsed likelihood computation linear algebra

--- src/linalg/chol.jl
+++ src/linalg/chol.jl
@@ -1,4 +1,6 @@
 
+using CUDA
+
 function ldl_striped_matrix!(A::Array, ϵ = eps(real(eltype(A))))
     @assert size(A,2) == size(A,3)
     B = size(A,1)
@@ -31,7 +33,68 @@ function ldl_striped_matrix!(A::Array, ϵ = eps(real(eltype(A))))
     D, A
 end
 
+function ldl_striped_matrix_gpu!(A::CuArray{T,3}, ϵ = eps(real(T))) where T<:Union{ComplexF32, ComplexF64}
+    
+    @assert size(A,2) == size(A,3)
+    B = size(A,1)
+    N = size(A,2)
+    D = CUDA.zeros(eltype(A), B, N)
+    buf = CUDA.zeros(eltype(A), B)
+
+    @inbounds for j in 1:N
+        Lj = view(A,1:B,j,1:j-1)
+        Dj = view(D,1:B,1:j-1)
+        @tullio buf[b] = abs2(Lj[b,k]) * Dj[b,k]
+        @. D[:,j] = A[:,j,j] - buf
+
+        A[:,j,j] .= one(eltype(A))
+
+        if any(abs.(D[:,j]) .≤ ϵ)
+            return nothing, nothing  # fail-fast on singular block
+        end
+
+        for i in j+1:N
+            Li = view(A,1:B,i,1:j-1)
+            @tullio buf[b] = Li[b,k] * conj(Lj[b,k]) * Dj[b,k]
+            @. A[:,i,j] = (A[:,i,j] - buf) / D[:,j]
+        end
+    end
+
+    @inbounds for j in 1:N, i in 1:j-1
+        A[:,i,j] .= zero(eltype(A))
+    end
+
+    return D, A
+end
+
 function cholesky_striped_matrix!(A::Array, ϵ = eps(real(eltype(A))))
+    @assert size(A,2) == size(A,3)
+    B = size(A,1)
+    N = size(A,2)
+    buf =CUDA.zeros(eltype(A), B)
+
+    for j in 1:N
+        Lj = view(A,1:B,j,1:j-1)
+        @tullio buf[b] = abs2(Lj[b,k])
+        A[:,j,j] = sqrt.(A[:,j,j] - buf)
+
+        if any(@. abs(A[:,j,j]) ≤ ϵ)
+            nothing
+        end
+        
+        @inbounds for i in j+1:N
+            Li = view(A,1:B,i,1:j-1)
+            @tullio buf[b] = conj(Lj[k])*Li[k]
+            A[:,i,j] = (A[:,i,j] - buf)./A[:,j,j]
+        end
+    end
+    @inbounds for j in 1:N, i in 1:j-1
+        A[:,i,j] .= zero(eltype(A))
+    end
+    A
+end
+
+function cholesky_striped_matrix_gpu!(A::CuArray{T,3}, ϵ = eps(real(T))) where T<:Union{ComplexF32, ComplexF64}
     @assert size(A,2) == size(A,3)
     B = size(A,1)
     N = size(A,2)
--- src/linalg/trsv.jl
+++ src/linalg/trsv.jl
@@ -14,3 +14,19 @@ function trsv_striped_matrix!(A::AbstractArray, x::Array)
     end
     x
 end
+
+function trsv_striped_matrix_gpu!(A::CudaArray{T,3}, x::Array)
+    @assert size(A,2) == size(A,3)
+    @assert size(A,2) == size(x,2)
+    @assert size(A,1) == size(x,1)
+    B   = size(A,1)
+    N   = size(A,2)
+    buf = CUDA.zeros(eltype(A), B)
+    @inbounds for i in 1:N
+        Ai = view(A, 1:B, i, 1:i-1)
+        xi = view(x, 1:B,    1:i-1)
+        @tullio buf[b] = Ai[b,k].*xi[b,k] 
+        x[:,i] = (x[:,i] - buf)./A[:,i,i]
+    end
+    x
+end
